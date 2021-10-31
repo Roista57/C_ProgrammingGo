0f31b30c03ce1007fcaee646ae059c2c4a6aae42
Roista57 <91594792+Roista57@users.noreply.github.com>
2021-10-31 18:40:23 +0900

    chapter 02

---
+++ chapter 02/2P_02.c
@@ -0,0 +1,10 @@
+// Write a program that outputs alphabetic lowercase letters a to z.
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i=97;i<123;i++){
+        printf("%c ", i);
+    }
+}
---
+++ chapter 02/2P_03.c
@@ -0,0 +1,10 @@
+// Write a program to obtain the area of the triangle by receiving the bottom l and height h as real values.
+
+#include <stdio.h>
+
+int main()
+{
+    float l, h;
+    scanf("%f %f", &l, &h);
+    printf("%.2f\n", l*h/2);
+}
